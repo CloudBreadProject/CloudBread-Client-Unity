439b004713c10b091498cb1074b6d34be9914fab
HONGYOONSEOK <yoonseok417@gmail.com>
2016-03-04 13:54:46 +0900

    finish socket.IO UI

--- Assets/Scripts/CloudBread/UI/CBSocketGUI.cs
+++ Assets/Scripts/CloudBread/UI/CBSocketGUI.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using SocketIO;
 
 public class CBSocketGUI : CBBaseUI {
+
 	SocketIOComponent socket;
 
 	// Use this for initialization
 	void Start () {
 		_chattingAreaRect = new Rect (MainAreaRect.width/2 - (500/2), MainAreaRect.y, 500, MainAreaRect.height);
-		for(int i = 0 ; i<100; i++)
-			addChattingList (new ChattingMessage{ userName = "홍 윤 석", content = "aadfjfdjif"});
+
 		GameObject go = GameObject.Find ("SocketIO");
 		this.socket = go.GetComponent<SocketIOComponent> ();
 		this.socket.On("authorized", (SocketIOEvent obj) => {
@@ -19,6 +19,17 @@ public class CBSocketGUI : CBBaseUI {
 		this.socket.On ("channel connected", (SocketIOEvent obj) => {
 			print (obj.ToString());
 		});
+	}
+
+	private void CreateChannel(){
+		Dictionary<string, string> channel = new Dictionary<string, string> ();
+		channel ["link"] = channelName;
+
+		this.socket.Emit ("join channel", new JSONObject(channel));
+
+		_chattingChannelList.Add (channelName);
+		_chattingDic.Add (channelName, new List<ChattingMessage> ());
+		_msgTemp.Add ("");
 	}
 
 	// Chatting Data Class
@@ -30,9 +41,8 @@ public class CBSocketGUI : CBBaseUI {
 
 
 	// send Button Clicked
-	private void sendBtnClicked(string Message){
-		addChattingList (new ChattingMessage{ userName = userName, content = Message});
-		scrollPosition.y = Mathf.Infinity;
+	private void sendBtnClicked(string channelName, string Message){
+		addChattingList (channelName, new ChattingMessage{ userName = userName, content = Message});
 
 		Dictionary<string, string> user = new Dictionary<string, string> ();
 		user ["username"] = this.userName;
@@ -40,71 +50,100 @@ public class CBSocketGUI : CBBaseUI {
 	}
 
 	private void UserSendBtnClicked(){
+		
 		Dictionary<string, string> user = new Dictionary<string, string> ();
 		user ["username"] = this.userName;
+		registerNameBool = true;
 		this.socket.Emit ("authenticate user", new JSONObject(user));
 	}
 
-	private void ChannalSendBtnClicked(){
-		Dictionary<string, string> channel = new Dictionary<string, string> ();
-		channel ["link"] = this.channelName;
+	private void ChannalSendBtnClicked(string channelName){
+		
+		// duplication check
+		var chattingChannelNameList = new List<string> (_chattingDic.Keys);
+		bool duplicateFlag = false;
+		foreach (var key in chattingChannelNameList) {
+			if (channelName.Equals (key))
+				duplicateFlag = true;
+		}
 
-		this.socket.Emit ("join channel", new JSONObject(channel));
+		if (!duplicateFlag)
+			CreateChannel ();
 	}
 
 	// add Chatting message in Chatting window
-	private void addChattingList(ChattingMessage msg){
-		_chattingList.Add (msg);
+	private void addChattingList(string channel, ChattingMessage msg){
+		_chattingDic [channel].Add (msg);
 	}
 
-	private string serverAddress = "";
+	private string getChannelStr (int i ){
+		return _chattingChannelList[i];
+	}
+
+	private void createChannel(string channelName){
+		_chattingChannelList.Add(channelName);
+		_chattingDic.Add (channelName, new List<ChattingMessage> ());
+	}
+
+	private int getChannelNum(){
+		return _chattingChannelList.Count;
+	}
+
 	private string userName = "";
 	private string channelName = "";
 
 	// Chating Message List
-	private List<ChattingMessage> _chattingList = new List<ChattingMessage> ();
+	private Dictionary<string, List<ChattingMessage>> _chattingDic = new Dictionary<string, List<ChattingMessage>> ();
+	private List<string> _chattingChannelList = new List<string>();
+	private List<string>_msgTemp = new List<string>();
 
-	private string _chattingMsg = "";
 	private Rect _chattingAreaRect;
+	private bool registerNameBool = false;
 
 	public void OnGUI()
 	{
 		GUILayout.BeginArea(_chattingAreaRect);
 		GUILayout.BeginVertical ();
-			// @TODO remove after authenticated
+		if (!registerNameBool) {
 			GUILayout.BeginHorizontal ("box");
-				GUILayout.Label ("User Name : ", GUILayout.Width(100));
+				GUILayout.Label ("User Name : ", GUILayout.Width (100));
 				userName = GUILayout.TextField (userName);
-				if(GUILayout.Button("SEND", GUILayout.Width(80))){
-					UserSendBtnClicked();
+				if (GUILayout.Button ("SEND", GUILayout.Width (80))) {
+					UserSendBtnClicked ();
 				}
 			GUILayout.EndHorizontal ();
+		} else {
+			scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (_chattingAreaRect.width), GUILayout.Height (_chattingAreaRect.height));
 			GUILayout.BeginHorizontal ("box");
-				GUILayout.Label ("Channel Name : ", GUILayout.Width(100));
+				GUILayout.Label ("Channel Name : ", GUILayout.Width (100));
 				channelName = GUILayout.TextField (channelName);
-				if(GUILayout.Button("SEND", GUILayout.Width(80))){
-					ChannalSendBtnClicked();
+				if (GUILayout.Button ("SEND", GUILayout.Width (80))) {
+					ChannalSendBtnClicked (channelName);
 				}
 			GUILayout.EndHorizontal ();
 
-			GUILayout.Label ("");
-
-			GUILayout.BeginVertical ("box");
-				scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(_chattingAreaRect.width), GUILayout.Height(_chattingAreaRect.height - 300));
-				foreach (var msg in _chattingList) {
+			for (int i = 0; i < getChannelNum(); i++){
+				GUILayout.Label ("");
+				GUILayout.Label (getChannelStr(i));
+				GUILayout.BeginVertical ("box");
+				foreach (var msg in _chattingDic[getChannelStr(i)]) {
 					GUILayout.BeginHorizontal ();
-						GUILayout.Label (msg.userName, GUILayout.Width (80));
-						GUILayout.TextArea (msg.content, GUILayout.Width(380));
+					GUILayout.Label (msg.userName, GUILayout.Width (80));
+					GUILayout.TextArea (msg.content, GUILayout.Width (380));
 					GUILayout.EndHorizontal ();
 				}
-				GUILayout.EndScrollView ();
 				GUILayout.BeginHorizontal ();
-					_chattingMsg = GUILayout.TextField (_chattingMsg);
-					if (GUILayout.Button ("Send", GUILayout.Width(50))) {
-						sendBtnClicked (_chattingMsg);
-					}
+
+				_msgTemp[i] = GUILayout.TextField (_msgTemp[i]);
+				if (GUILayout.Button ("Send", GUILayout.Width (50))) {
+					sendBtnClicked (getChannelStr(i), _msgTemp[i]);
+					_msgTemp [i] = "";
+				}
 				GUILayout.EndHorizontal ();
 			GUILayout.EndVertical ();
+			}
+			GUILayout.EndScrollView ();
+		}
 		GUILayout.EndVertical ();
 		GUILayout.EndArea ();
 	}
