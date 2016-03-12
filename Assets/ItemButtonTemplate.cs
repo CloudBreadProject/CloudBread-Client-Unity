1c5ea78809e1353405665432dfe4cf20e9575e95
HONGYOONSEOK <yoonseok417@gmail.com>
2016-03-12 22:51:39 +0900

    complete test (add Item Scrollview Grid)

---
+++ Assets/ItemButtonTemplate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ItemButtonTemplate : MonoBehaviour {
+
+	private string Name;
+	public Text ButtonText;
+	public ScrollViewTemplate ScrollView;
+
+	public void SetName(string name)
+	{
+		Name = name;
+		ButtonText.text = name;
+	}
+
+	public void Button_Click()
+	{
+		ScrollView.ButtonClicked(Name);
+
+	}
+
+}
---
+++ Assets/ScrollViewTemplate.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScrollViewTemplate : MonoBehaviour {
+
+	public GameObject Button_Template;
+	private List<string> NameList = new List<string>();
+
+
+	// Use this for initialization
+	void Start () {
+
+		NameList.Add("Alan");
+		NameList.Add("Amy");
+		NameList.Add("Brian");
+		NameList.Add("Carrie");
+		NameList.Add("David");
+		NameList.Add("Joe");
+		NameList.Add("Jason");
+		NameList.Add("Michelle");
+		NameList.Add("Stephanie");
+		NameList.Add("Zoe");
+
+		NameList.Add("Alan");
+		NameList.Add("Amy");
+		NameList.Add("Brian");
+		NameList.Add("Carrie");
+		NameList.Add("David");
+		NameList.Add("Joe");
+		NameList.Add("Jason");
+		NameList.Add("Michelle");
+		NameList.Add("Stephanie");
+		NameList.Add("Zoe");
+
+		foreach(string str in NameList)
+		{
+			GameObject go = Instantiate(Button_Template) as GameObject;
+			go.SetActive(true);
+			ItemButtonTemplate TB = go.GetComponent<ItemButtonTemplate>();
+			TB.SetName(str);
+			go.transform.SetParent(Button_Template.transform.parent);
+
+		}
+
+
+	}
+
+	public void ButtonClicked(string str)
+	{
+		Debug.Log(str + " button clicked.");
+
+	}
+}
