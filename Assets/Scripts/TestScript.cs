b6dabdd47ff8d14ceeaf9b951757f55281b93b59
HONGYOONSEOK <yoonseok417@gmail.com>
2016-03-31 04:52:25 +0900

    only Json Test

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -3,11 +3,19 @@ using System.Collections;
 using AssemblyCSharp;
 
 public class TestScript : MonoBehaviour {
-
+	string jsonTest = "[{\"noticeID\":\"NoticeID1\",\"noticeCategory1\":\"NoticeCategory1\",\"noticeCategory2\":\"NoticeCategory2\",\"noticeCategory3\":\"NoticeCategory3\",\"targetGroup\":\"TargetGroup\",\"targetOS\":\"TargetOS\",\"targetDevice\":\"TargetDevice\",\"noticeImageLink\":\"NoticeImageLink\",\"title\":\"title1\",\"content\":\"content\",\"sCol1\":\"sCol1\",\"sCol2\":\"sCol2\",\"sCol3\":\"sCol3\",\"sCol4\":\"sCol4\",\"sCol5\":\"sCol5\",\"sCol6\":\"sCol6\",\"sCol7\":\"sCol7\",\"sCol8\":\"sCol8\",\"sCol9\":\"sCol9\",\"sCol10\":\"sCol10\"},{\"noticeID\":\"NoticeID2\",\"noticeCategory1\":\"NoticeCategory1\",\"noticeCategory2\":\"NoticeCategory2\",\"noticeCategory3\":\"NoticeCategory3\",\"targetGroup\":\"TargetGroup\",\"targetOS\":\"TargetOS\",\"targetDevice\":\"TargetDevice\",\"noticeImageLink\":\"NoticeImageLink\",\"title\":\"title2\",\"content\":\"content\",\"sCol1\":\"sCol1\",\"sCol2\":\"sCol2\",\"sCol3\":\"sCol3\",\"sCol4\":\"sCol4\",\"sCol5\":\"sCol5\",\"sCol6\":\"sCol6\",\"sCol7\":\"sCol7\",\"sCol8\":\"sCol8\",\"sCol9\":\"sCol9\",\"sCol10\":\"sCol10\"},{\"noticeID\":\"NoticeID3\",\"noticeCategory1\":\"NoticeCategory1\",\"noticeCategory2\":\"NoticeCategory2\",\"noticeCategory3\":\"NoticeCategory3\",\"targetGroup\":\"TargetGroup\",\"targetOS\":\"TargetOS\",\"targetDevice\":\"TargetDevice\",\"noticeImageLink\":\"NoticeImageLink\",\"title\":\"title3\",\"content\":\"content\",\"sCol1\":\"sCol1\",\"sCol2\":\"sCol2\",\"sCol3\":\"sCol3\",\"sCol4\":\"sCol4\",\"sCol5\":\"sCol5\",\"sCol6\":\"sCol6\",\"sCol7\":\"sCol7\",\"sCol8\":\"sCol8\",\"sCol9\":\"sCol9\",\"sCol10\":\"sCol10\"},{\"noticeID\":\"NoticeID4\",\"noticeCategory1\":\"NoticeCategory1\",\"noticeCategory2\":\"NoticeCategory2\",\"noticeCategory3\":\"NoticeCategory3\",\"targetGroup\":\"TargetGroup\",\"targetOS\":\"TargetOS\",\"targetDevice\":\"TargetDevice\",\"noticeImageLink\":\"NoticeImageLink\",\"title\":\"title4\",\"content\":\"content\",\"sCol1\":\"sCol1\",\"sCol2\":\"sCol2\",\"sCol3\":\"sCol3\",\"sCol4\":\"sCol4\",\"sCol5\":\"sCol5\",\"sCol6\":\"sCol6\",\"sCol7\":\"sCol7\",\"sCol8\":\"sCol8\",\"sCol9\":\"sCol9\",\"sCol10\":\"sCol10\"}]";
 	// Use this for initialization
 	void Start () {
 		TestString = CBAuthentication.AES_decrypt (token, keyValue, keyValue);
 
+		Notice [] notice = (Notice[]) JsonFx.Json.JsonReader.Deserialize<Notice[]> (jsonTest);
+
+		print ("======================================");
+		print (notice [0].id);
+
+		string str = JsonFx.Json.JsonWriter.Serialize(notice);
+		print ("======================================");
+		print (str);
 	}
 	
 	// Update is called once per frame
@@ -27,3 +35,57 @@ public class TestScript : MonoBehaviour {
 		GUILayout.EndVertical ();
 	}
 }
+
+//[System.Serializable]
+public class Notice {
+
+//	[JsonProperty(PropertyName = "FooBar")]
+
+	[JsonFx.Json.JsonName("noticeID")]
+	public string id;
+
+	public string noticeCategory1;
+	public string noticeCategory2;
+	public string noticeCategory3;
+	public string targetGroup;
+	public string targetOS;
+	public string targetDevice;
+	public string noticeImageLink;
+	public string title;
+	public string content;
+	public string sCol1;
+	public string sCol2;
+	public string sCol3;
+	public string sCol4;
+	public string sCol5;
+	public string sCol6;
+	public string sCol7;
+	public string sCol8;
+	public string sCol9;
+	public string sCol10;
+
+	[JsonFx.Json.JsonIgnore]
+	public string a;
+}
+//{
+//	"noticeID": "NoticeID1",
+//	"noticeCategory1": "NoticeCategory1",
+//	"noticeCategory2": "NoticeCategory2",
+//	"noticeCategory3": "NoticeCategory3",
+//	"targetGroup": "TargetGroup",
+//	"targetOS": "TargetOS",
+//	"targetDevice": "TargetDevice",
+//	"noticeImageLink": "NoticeImageLink",
+//	"title": "title1",
+//	"content": "content",
+//	"sCol1": "sCol1",
+//	"sCol2": "sCol2",
+//	"sCol3": "sCol3",
+//	"sCol4": "sCol4",
+//	"sCol5": "sCol5",
+//	"sCol6": "sCol6",
+//	"sCol7": "sCol7",
+//	"sCol8": "sCol8",
+//	"sCol9": "sCol9",
+//	"sCol10": "sCol10"
+//},
