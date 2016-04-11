c7be9308163f6b57b553bbba95c58f07e0e7c293
HONGYOONSEOK <yoonseok417@gmail.com>
2016-04-12 03:15:52 +0900

    AzureMobileAppRequestHelper refactoring not used header

--- Assets/Scripts/CloudBread/AzureMobileAppRequestHelper.cs
+++ Assets/Scripts/CloudBread/AzureMobileAppRequestHelper.cs
@@ -1,16 +1,11 @@
 ﻿using UnityEngine;
 using System;
-using System.Text;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace AssemblyCSharp
 {
 	public class AzureMobileAppRequestHelper
 	{
-
-		// Header Default
-
 		// ZUMO-API-VERSION
 		private static string _api_version = "2.0.0";
 
@@ -29,15 +24,6 @@ namespace AssemblyCSharp
 			cbAuth = new CBAuthentication ();
 		}
 
-//		public string setServerAdd(string address){
-//			char lastChar = address [address.Length];
-//			if (lastChar.CompareTo ("/") == 0) {
-//				return address.Substring (0, address.Length - 1);
-//			};
-//
-//			return address;
-//		}
-
 		public void setTokenJson(string result){
 			Debug.Log ("[AzureMobileAppRequest] " + result);
 			var resultDic = (Dictionary<string, object>) JsonParser.Read2Object(result);
@@ -46,23 +32,6 @@ namespace AssemblyCSharp
 			var crypt = CBAuthentication.AES_decrypt (cbAuth.token, "1234567890123456", "1234567890123456");
 
 			Debug.Log ("[token decrypt] " + crypt);
-		}
-
-		public Dictionary<string, string> getDefaultHeader(){
-			var header = new Dictionary<string, string> (); 
-			header["ZUMO-API-VERSION"] = _api_version;
-			header ["Accept"] = "application/json";
-
-			header["X-ZUMO-VERSION"] = "ZUMO/2.0 (lang=Managed; os=Windows Store; os_version=--; arch=X86; version=2.0.31217.0)";
-			header ["X-ZUMO-FEATURES"] = "AJ";
-			header ["X-ZUMO-INSTALLATION-ID"] = "fe52b710-0312-4cad-8d53-dfd28d4c6f9b";
-			header ["Content-Type"] = "application/json";
-
-			if (AuthToken != null) {
-				header ["X-ZUMO-AUTH"] = AuthToken;
-			}
-		
-			return header;
 		}
 
 		public static Dictionary<string, string> getHeader(){
