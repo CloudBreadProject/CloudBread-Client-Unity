9240b2e3fe54a27e506d56961e25473dcb2a0eb7
HONGYOONSEOK <yoonseok417@gmail.com>
2016-05-02 15:22:19 +0900

    Change Json Library

--- Assets/Scripts/CloudBread/JsonParser.cs
+++ Assets/Scripts/CloudBread/JsonParser.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.IO;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using JsonFx.Json;
+// Json Library Change
 
 namespace AssemblyCSharp
 {
@@ -13,37 +15,31 @@ namespace AssemblyCSharp
 		}
 
 		public static string Write(object obj){
-			return JsonFx.Json.JsonWriter.Serialize (obj);
+			return JsonUtility.ToJson(obj);
+//			return JsonFx.Json.JsonWriter.Serialize (obj);
 		}
 
 		public static string Write(Dictionary<string, object> obj){
-			return JsonWriter.Serialize (obj);
+			return JsonUtility.ToJson (obj);
+//			return JsonWriter.Serialize (obj);
 		}
 
 		public static string WritePretty(Dictionary<string,object>[] obj){
-
-			JsonWriterSettings settings = new JsonWriterSettings();
-			settings.PrettyPrint = true;
-			JsonFx.Json.JsonDataWriter writer = new JsonDataWriter(settings);
-
-			StringWriter wr = new StringWriter();
-			writer.Serialize(wr, obj);
-
-			string json = wr.ToString();
-
-			return json;
+			return WritePretty (obj);;
 		}
 
 		public static string WritePretty(object obj){
 
-			JsonWriterSettings settings = new JsonWriterSettings();
-			settings.PrettyPrint = true;
-			JsonFx.Json.JsonDataWriter writer = new JsonDataWriter(settings);
+//			JsonWriterSettings settings = new JsonWriterSettings();
+//			settings.PrettyPrint = true;
+//			JsonFx.Json.JsonDataWriter writer = new JsonDataWriter(settings);
+//
+//			StringWriter wr = new StringWriter();
+//			writer.Serialize(wr, obj);
 
-			StringWriter wr = new StringWriter();
-			writer.Serialize(wr, obj);
+//			string json = wr.ToString();
 
-			string json = wr.ToString();
+			string json = JsonUtility.ToJson (obj);
 
 			return json;
 		}
