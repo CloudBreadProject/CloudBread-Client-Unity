774cc52369a7ce98ea655315d92aa11388ec46d5
HONGYOONSEOK <yoonseok417@gmail.com>
2016-05-04 01:29:29 +0900

    change ui comfortable

--- Assets/Scripts/CloudBreadTestUI.cs
+++ Assets/Scripts/CloudBreadTestUI.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 using AssemblyCSharp;
-using JsonFx;
 
 public class CloudBreadTestUI : MonoBehaviour {
-	private string ServerAddress = "https://cb2-auth-demo.azurewebsites.net/";
+	private string ServerAddress = "https://cb2-demo.azurewebsites.net/";
 
 	private enum TESTPAGE {main, login, authentication, userInfo, memberGameInfo, itemList, userItem, gameStage, notice, events, coupons, socket, encypt};
 
@@ -17,62 +16,65 @@ public class CloudBreadTestUI : MonoBehaviour {
 		MainContainer.name = "MainView";
 	}
 
+	public int buttonHeight = 50;
+	public int buttonWidth = 120;
+
 	public void OnGUI()
 	{
 		
 		GUILayout.BeginHorizontal ();
-		GUILayout.BeginArea (new Rect (new Vector2 (Screen.width - 120, 0), new Vector2 (120, Screen.height)));
+		GUILayout.BeginArea (new Rect (new Vector2 (Screen.width - buttonWidth, 0), new Vector2 (buttonWidth, Screen.height)));
 		GUILayout.BeginVertical ("box");
-		if (GUILayout.Button ("Encypt", GUILayout.Height (50))) {
+		if (GUILayout.Button ("Encypt", GUILayout.Height (buttonHeight))) {
 			print ("Main Page");
 			PresentationView (TESTPAGE.encypt);
 		}
-		if (GUILayout.Button ("메인 페이지", GUILayout.Height (50))) {
+		if (GUILayout.Button ("메인 페이지", GUILayout.Height (buttonHeight))) {
 			print ("Main Page");
 			PresentationView (TESTPAGE.main);
 		}
-		if (GUILayout.Button ("로그인 페이지", GUILayout.Height (50))) {
+		if (GUILayout.Button ("로그인 페이지", GUILayout.Height (buttonHeight))) {
 			print ("Login Page");
 			PresentationView (TESTPAGE.login);
 		}
-		if (GUILayout.Button ("인증 후 이름 \n 메일 저장", GUILayout.Height (50))) {
+		if (GUILayout.Button ("인증 후 이름 \n 메일 저장", GUILayout.Height (buttonHeight))) {
 			print ("Authentication Page");
 			PresentationView (TESTPAGE.authentication);
 
 		}
-		if (GUILayout.Button ("회원 정보", GUILayout.Height (50))) {
+		if (GUILayout.Button ("회원 정보", GUILayout.Height (buttonHeight))) {
 			print ("UserInfo Page");
 			PresentationView (TESTPAGE.userInfo);
 		}
-		if (GUILayout.Button ("게임 정보", GUILayout.Height (50))) {
+		if (GUILayout.Button ("게임 정보", GUILayout.Height (buttonHeight))) {
 			print ("GameInfo Page");
 			PresentationView (TESTPAGE.memberGameInfo);
 		}
-		if (GUILayout.Button ("아이템 목록", GUILayout.Height (50))) {
+		if (GUILayout.Button ("아이템 목록", GUILayout.Height (buttonHeight))) {
 			print ("ItemList Page");
 			PresentationView (TESTPAGE.itemList);
 		}
-		if (GUILayout.Button ("게이머 소유\n아이템 목록", GUILayout.Height (50))) {
+		if (GUILayout.Button ("게이머 소유\n아이템 목록", GUILayout.Height (buttonHeight))) {
 			print ("UserItem Page");
 			PresentationView (TESTPAGE.userItem);
 		}
-		if (GUILayout.Button ("게임 스테이지", GUILayout.Height (50))) {
+		if (GUILayout.Button ("게임 스테이지", GUILayout.Height (buttonHeight))) {
 			print ("GameStage Page");
 			PresentationView (TESTPAGE.gameStage);
 		}
-		if (GUILayout.Button ("공지사항", GUILayout.Height (50))) {
+		if (GUILayout.Button ("공지사항", GUILayout.Height (buttonHeight))) {
 			print ("Notice Page");
 			PresentationView (TESTPAGE.notice);
 		}
-		if (GUILayout.Button ("이벤트", GUILayout.Height (50))) {
+		if (GUILayout.Button ("이벤트", GUILayout.Height (buttonHeight))) {
 			print ("Events Page");
 			PresentationView (TESTPAGE.events);
 		}
-		if (GUILayout.Button ("쿠폰", GUILayout.Height (50))) {
+		if (GUILayout.Button ("쿠폰", GUILayout.Height (buttonHeight))) {
 			print ("Coupons Page");
 			PresentationView (TESTPAGE.coupons);
 		}
-		if (GUILayout.Button ("Socket 채팅", GUILayout.Height (50))) {
+		if (GUILayout.Button ("Socket 채팅", GUILayout.Height (buttonHeight))) {
 			print ("Socket Page");
 			PresentationView (TESTPAGE.socket);
 		}
