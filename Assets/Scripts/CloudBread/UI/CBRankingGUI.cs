920fd314d8279509950289056bf46acd05649fae
HONGYOONSEOK <yoonseok417@gmail.com>
2016-05-12 02:58:20 +0900

    change Ranking ui

--- Assets/Scripts/CloudBread/UI/CBRankingGUI.cs
+++ Assets/Scripts/CloudBread/UI/CBRankingGUI.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
 using System.Collections;
 
 public class CBRankingGUI : MonoBehaviour {
@@ -15,77 +17,49 @@ public class CBRankingGUI : MonoBehaviour {
 	public float RealHeight = 0.0f;
 	public float RealDip = 0.0f;
 
-	ArrayList RankingList;
+	public RankingRow[] RankingList;
 
 	public Rect rect;
 
-	struct RankingRow {
-		public string NickName;
-		public string Score;
+	public GUIText myRankGUIText;
+	public GameObject myNameGUIText;
+	public GUIText myScoreGUIText;
+
+//	public ArrayList Ranking = new ArrayList ();
+
+	string testJson = "[{\"element\": \"415ee4a6338615d682e16bb7e6b56f05\",\"score\": 57,\"value\": 57,\"key\": \"415ee4a6338615d682e16bb7e6b56f05\"}]";
+
+	[Serializable]
+	public struct RankingRow {
+		[SerializeField]
+		public string element;
+
+		[SerializeField]
+		public string score;
+
+		[SerializeField]
+		public string value;
+
+		[SerializeField]
+		public string key;
 	}
+
 
 	public void setRankingData(){
 
 	}
 		
+
 	// Use this for initialization
 	void Start () {
-//		#if UNITY_ANDROID
-//			matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3(Screen.width/virtualWidth, Screen.height/virtualHeight, 1.0f));
-		matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3(Screen.width/virtualWidth, Screen.height/virtualHeight, 1.0f));
-//		#endif
+		var a = GameObject.Find ("MyNameText").GetComponent<Text>();
+		a.text = "Hong";
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+		
 
-	private Vector2 ScrollVec= new Vector2();
-
-	public GUIContent contet;
-
-	void OnGUI(){
-		RealWidth = Screen.width;
-		RealHeight = Screen.height;
-		RealDip = Screen.dpi;
-		GUI.matrix = matrix;
-
-		GUI.BeginGroup (new Rect (10, 10, Screen.width - 10, Screen.height - 10));
-
-		GUILayout.BeginVertical ();
-
-		GUILayout.BeginHorizontal ();
-		GUILayout.Label ("Ranking");
-		GUILayout.EndHorizontal ();
-
-		GUILayout.Label (" ");
-
-		GUILayout.BeginHorizontal ();
-		GUILayout.Label ("나의 등수 : ");
-		GUILayout.Label ("100");
-		GUILayout.Label ("등");
-		GUILayout.EndHorizontal ();
-
-		GUILayout.BeginHorizontal ();
-		GUILayout.Label ("100");
-
-		GUILayout.EndHorizontal ();
-
-		GUILayout.BeginScrollView (ScrollVec);
-		for (int i = 0; i < 10; i++) {
-			GUILayout.BeginHorizontal ();
-			GUILayout.Label ((i+1).ToString () + ". ");
-			GUILayout.Label ("Nick Name");
-			GUILayout.Label ("Score");
-			GUILayout.EndHorizontal ();
-		}
-		GUILayout.EndScrollView ();
-
-		GUILayout.EndVertical ();
-
-		GUI.EndGroup ();
-	}
-
-	
 }
