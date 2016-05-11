94d9b2721f414585223049f1106089874357ab03
HONGYOONSEOK <yoonseok417@gmail.com>
2016-05-11 21:32:53 +0900

    make ranking ui

--- Assets/Scripts/CloudBread/UI/CBRankingGUI.cs
+++ Assets/Scripts/CloudBread/UI/CBRankingGUI.cs
@@ -7,11 +7,17 @@ public class CBRankingGUI : MonoBehaviour {
 //	float virtualHeight = 1080.0f;
 
 
-	float virtualWidth = 800.0f;
-	float virtualHeight = 480.0f;
+	public float virtualWidth = 800.0f;
+	public float virtualHeight = 480.0f;
 	Matrix4x4 matrix;
 
+	public float RealWidth = 0.0f;
+	public float RealHeight = 0.0f;
+	public float RealDip = 0.0f;
+
 	ArrayList RankingList;
+
+	public Rect rect;
 
 	struct RankingRow {
 		public string NickName;
@@ -21,11 +27,12 @@ public class CBRankingGUI : MonoBehaviour {
 	public void setRankingData(){
 
 	}
-
+		
 	// Use this for initialization
 	void Start () {
 //		#if UNITY_ANDROID
-			matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3(Screen.width/virtualWidth, Screen.height/virtualHeight, 1.0f));
+//			matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3(Screen.width/virtualWidth, Screen.height/virtualHeight, 1.0f));
+		matrix = Matrix4x4.TRS (Vector3.zero, Quaternion.identity, new Vector3(Screen.width/virtualWidth, Screen.height/virtualHeight, 1.0f));
 //		#endif
 	}
 	
@@ -34,8 +41,14 @@ public class CBRankingGUI : MonoBehaviour {
 	
 	}
 
+	private Vector2 ScrollVec= new Vector2();
+
+	public GUIContent contet;
+
 	void OnGUI(){
-//		#
+		RealWidth = Screen.width;
+		RealHeight = Screen.height;
+		RealDip = Screen.dpi;
 		GUI.matrix = matrix;
 
 		GUI.BeginGroup (new Rect (10, 10, Screen.width - 10, Screen.height - 10));
@@ -48,6 +61,18 @@ public class CBRankingGUI : MonoBehaviour {
 
 		GUILayout.Label (" ");
 
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label ("나의 등수 : ");
+		GUILayout.Label ("100");
+		GUILayout.Label ("등");
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginHorizontal ();
+		GUILayout.Label ("100");
+
+		GUILayout.EndHorizontal ();
+
+		GUILayout.BeginScrollView (ScrollVec);
 		for (int i = 0; i < 10; i++) {
 			GUILayout.BeginHorizontal ();
 			GUILayout.Label ((i+1).ToString () + ". ");
@@ -55,14 +80,12 @@ public class CBRankingGUI : MonoBehaviour {
 			GUILayout.Label ("Score");
 			GUILayout.EndHorizontal ();
 		}
+		GUILayout.EndScrollView ();
 
 		GUILayout.EndVertical ();
 
 		GUI.EndGroup ();
 	}
 
-	void drawRow(){
-
-	}
 	
 }
