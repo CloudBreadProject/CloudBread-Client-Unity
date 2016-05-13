9b620f9b47c4eb63c860b4bd4702a749c25b30ea
HONGYOONSEOK <yoonseok417@gmail.com>
2016-05-13 16:23:02 +0900

    chatting scene

--- Assets/CloudBread_Sample/Examples/SocketChatting/Scripts/CBChattingScripts.cs
+++ Assets/CloudBread_Sample/Examples/SocketChatting/Scripts/CBChattingScripts.cs
@@ -11,8 +11,32 @@ public class CBChattingScripts : MonoBehaviour {
 
 //	public Text text;
 
+	public struct ChatData
+	{
+		public string userName;
+		public string text;
+
+	};
+
+
+	public GameObject Chatting_Panel;
+	public GameObject Chatting_Chat;
+
 	// Use this for initialization
 	void Start () {
+
+		for (int i = 0; i < 100; i++) {
+//			var a = Instantiate (Chatting_Chat) as GameObject;
+//
+//			a.transform.SetParent (Chatting_Panel.transform, false);
+			addChattingScript (new ChatData {
+				userName = "hong",
+				text = "Hello1"
+			});
+		}
+
+//		Chatting_Panel.transform.GetComponent<RectTransform> ().rect.height = 30 * 100;
+
 //		Text t = ContnetChatting.GetComponent<Text> ();
 
 //		GameObject childObject = Instantiate(GameObject.Find("Chat_Panel")) as GameObject;
@@ -41,5 +65,24 @@ public class CBChattingScripts : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+		
+	public void addChattingScript(ChatData data){
+		var chatRow = Instantiate (Chatting_Chat) as GameObject;
+
+		var nameGObj = chatRow.transform.FindChild ("Name_Text") ;
+		var contentGObj = chatRow.transform.FindChild ("Background_Img").transform.FindChild("Content_Text");
+
+		nameGObj.GetComponent<Text> ().text = data.userName;
+		contentGObj.GetComponent<Text> ().text = data.text;
+
+		chatRow.transform.SetParent (Chatting_Panel.transform, false);
+
+		var cellsize = Chatting_Panel.transform.GetComponent<GridLayoutGroup> ().cellSize;
+		var chatContentHeight = Chatting_Panel.transform.childCount * cellsize.y;
+//		print (a);
+
+		Chatting_Panel.GetComponent<RectTransform> ().sizeDelta = new Vector2 ( 1, chatContentHeight);
+//		Chatting_Panel
 	}
 }
